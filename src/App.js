b045d6e3dc90ccc0c44166215d14c092f976dd26
Amee <ameecova@yahoo.com>
2020-06-02 12:46:43 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Pasos
         </p>
         <a
           className="App-link"
@@ -20,7 +21,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
