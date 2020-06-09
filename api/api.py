4fc2f629679a74ebd3a6c6688afdae7a863b8ccc
Amee <ameecova@yahoo.com>
2020-06-08 17:11:25 -0700

    working front end and back end

--- api/api.py
+++ api/api.py
@@ -3,11 +3,15 @@
 import time
 from flask import (Flask, request, flash, session, redirect)
 from model import db, connect_to_db
-from flask_oauth import OAuth
+# from flask_oauth import OAuth
 import crud
 
 app = Flask(__name__)
 
+@app.route('/time')
+def get_current_time():
+    print('test')
+    return {'time': time.time()}
 
 
 if __name__ == '__main__':
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from 'react';
 import "./App.css";
 import ReactDom from "react-dom";
 import GoogleLogin from 'react-google-login';
@@ -7,12 +7,22 @@ const responseGoogle = (response) => {
   console.log(response);
 }
 
-class App extends Component {
-  render() {
+function App() {
+    const [currentTime, setCurrentTime] = useState(0);
+
+    useEffect(() => {
+      console.log('test')
+      fetch('/time', {headers: {
+    'Accept': 'application/json'
+  }}).then(res => res.json()).then(data => {
+        setCurrentTime(data.time);
+      });
+    }, []);
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Pasos</h1>
+          <p>The current time is {currentTime}.</p>
         </header>
         <p className="App-intro">
           A virtual pilgrimmage on the Camino de Santiago
@@ -28,6 +38,5 @@ class App extends Component {
       </div>
     );
   }
-}
 
 export default App;
